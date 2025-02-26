2ba5182c91e22ba6d22ce9148accaa7966eae6ae
JACQUET Vincent <vincent.jacquet@alterway.fr>
2025-02-26 16:49:10 +0100

    fix: hashrouter pour que mariage soit dispo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { PhotoPage } from './pages/PhotoPage';
 import { CinemaPage } from './pages/CinemaPage';
 import { BlogPage } from './pages/BlogPage';
